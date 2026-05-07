79397033def9c08413923c89bbc62bdcb7b8f85e
Heitor Costa Silva <heitorcostasilva5@gmail.com>
2026-05-06 21:47:13 -0300

    chore: configurar projeto Django

---
+++ backend/papellog/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('auth/login', TokenObtainPairView.as_view(), name='auth_login'),
+    path('auth/refresh', TokenRefreshView.as_view(), name='auth_refresh'),
+    path('', include('core.urls')),
+]
